95b3a19b08f6e63a8e02ae9355d1960af0e51fc1
remax21 <deronzier.remi@gmail.com>
2023-09-19 16:35:47 +0200

    feat(tailwind): add tailwind form lib

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import tailwindcssForm from "@tailwindcss/forms";
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcssForm],
+} satisfies Config;
