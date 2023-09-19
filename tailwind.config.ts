30b8d3974b0568111045ecf4777c96dfb53f8af3
remax21 <deronzier.remi@gmail.com>
2023-09-19 16:37:32 +0200

    feat(tailwind): make inter the default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,27 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
   },
   plugins: [tailwindcssForm],
 } satisfies Config;
