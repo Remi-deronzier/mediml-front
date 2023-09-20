717e6db7de2b09a17421dfaed0f4043d276ec0b1
remax21 <deronzier.remi@gmail.com>
2023-09-20 10:40:40 +0200

    feat(strokeView): make description for the radio group mandatory

--- src/features/stroke/view/StrokeView.tsx
+++ src/features/stroke/view/StrokeView.tsx
@@ -27,6 +27,7 @@ export default function StrokeView() {
   return (
     <AppForm form={form}>
       <StrokeRadioGroup
+        description="What is the gender of the patient?"
         groupName="gender"
         values={genderChoices}
         label="Gender"
@@ -34,31 +35,36 @@ export default function StrokeView() {
       <AppInput<StrokeSchemaType>
         label="Age"
         commonName="age"
-        placeholder="Age of the patient"
+        placeholder="42"
         type="number"
         step={1}
       />
       <StrokeRadioGroup
+        description="Does the patient have hypertension?"
         groupName="hypertension"
         label="Hypertension"
         values={hypertensionChoices}
       />
       <StrokeRadioGroup
+        description="Does the patient have heart disease?"
         groupName="heartDisease"
         label="Heart disease"
         values={heartDiseaseChoices}
       />
       <StrokeRadioGroup
+        description="Has the patient ever been married?"
         groupName="everMarried"
         label="Married"
         values={everMarriedChoices}
       />
       <StrokeRadioGroup
+        description="What is the work type of the patient?"
         groupName="workType"
         label="Work type"
         values={workTypeChoices}
       />
       <StrokeRadioGroup
+        description="What is the residence type of the patient?"
         groupName="residenceType"
         label="Residence type"
         values={residenceTypeChoices}
@@ -66,16 +72,17 @@ export default function StrokeView() {
       <AppInput<StrokeSchemaType>
         label="Average glucose level"
         commonName="avgGlucoseLevel"
-        placeholder="Average glucose level of the patient"
+        placeholder="42.42"
         type="number"
       />
       <AppInput<StrokeSchemaType>
         label="BMI"
         commonName="bmi"
-        placeholder="BMI of the patient"
+        placeholder="42.42"
         type="number"
       />
       <StrokeRadioGroup
+        description="Does the patient smoke?"
         groupName="smokingStatus"
         label="Smoking status"
         values={smokingStatusChoices}
--- src/features/stroke/view/components/StrokeRadioGroup.tsx
+++ src/features/stroke/view/components/StrokeRadioGroup.tsx
@@ -6,13 +6,20 @@ import AppRadioGroup from '@components/RadioGroup';
 interface Props {
   groupName: Extract<keyof StrokeSchemaType, string>;
   label: string;
+  description: string;
   values: RadioChoice[];
 }
 
-export default function StrokeRadioGroup({ groupName, label, values }: Props) {
+export default function StrokeRadioGroup({
+  groupName,
+  label,
+  values,
+  description,
+}: Props) {
   return (
     <AppRadioGroup<RadioChoice, StrokeSchemaType>
       label={label}
+      description={description}
       values={values}
       defaultValue={
         values.find((choice) => choice.isDefault === true) ?? values[0]
