f81385190dd1f5f3b7dceb9b76e6fbfa608cd01f
remax21 <deronzier.remi@gmail.com>
2023-09-20 11:17:41 +0200

    feat(strokeView): reduce max width for the form

--- src/features/stroke/view/StrokeView.tsx
+++ src/features/stroke/view/StrokeView.tsx
@@ -26,7 +26,7 @@ export default function StrokeView() {
   });
 
   return (
-    <AppForm form={form} fieldsetClassName="space-y-8">
+    <AppForm form={form} fieldsetClassName="space-y-8 max-w-xl mx-auto">
       <StrokeRadioGroup
         description="What is the gender of the patient?"
         groupName="gender"
