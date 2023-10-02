2d27bf5270e13466890571c5a11f7cdd05a746c6
remax21 <deronzier.remi@gmail.com>
2023-10-02 22:41:53 +0200

    fix logo in home page

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,33 +1,34 @@
+import { FaBrain, FaHeartbeat } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { FaHeartbeat, FaBrain } from 'react-icons/fa';
+
+import Logo from '@assets/images/logo.svg?react';
 
 export default function Main() {
   return (
-    <div className="text-center p-8">
+    <div className="p-8 text-center">
       <div>
         <div className="h-80vh flex items-center justify-center">
-          <img
-            src="src/assets/images/logo.svg"
-            alt="Logo"
-            className="max-w-80%"
-          />
+          <Logo />
         </div>
       </div>
 
-      <h2 className="text-xl font-bold my-4">
+      <h2 className="my-4 text-xl font-bold">
         Welcome to MediML, your trustworthy AI for medical purposes
       </h2>
 
       <div className="mt-8 flex justify-center space-x-8">
         <Link to="/stroke" className="text-blue-500 hover:text-blue-700">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full flex items-center space-x-2">
+          <button className="flex items-center space-x-2 rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-700">
             <span>Stroke</span>
             <FaBrain />
           </button>
         </Link>
 
-        <Link to="/cardiovascular" className="text-blue-500 hover:text-blue-700">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full flex items-center space-x-2">
+        <Link
+          to="/cardiovascular"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          <button className="flex items-center space-x-2 rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-700">
             <span>Cardiovascular</span>
             <FaHeartbeat />
           </button>
